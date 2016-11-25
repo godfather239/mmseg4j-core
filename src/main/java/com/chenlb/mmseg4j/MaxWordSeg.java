69bdb226779a12d1c09d880fe1c6323e8a274f1c
godfather239 <greenday239@sina.com>
2016-11-25 18:12:59 +0800

    modify

--- src/main/java/com/chenlb/mmseg4j/MaxWordSeg.java
+++ src/main/java/com/chenlb/mmseg4j/MaxWordSeg.java
@@ -22,9 +22,9 @@ public class MaxWordSeg extends ComplexSeg {
 			for(int i=0; i<chunk.getCount(); i++) {
 				Word word = chunk.words[i];
 
-				if(word.getLength() < 3) {
-					cks.add(word);
-				} else {
+				// strategy by wenjieg
+				cks.add(word);
+				if (word.getLength() >= 3) {
 					char[] chs = word.getSen();
 					int offset = word.getWordOffset(), n = 0, wordEnd = word.getWordOffset()+word.getLength();
 					int senStartOffset = word.getStartOffset() - offset;	//sen 在文件中的位置
@@ -45,6 +45,30 @@ public class MaxWordSeg extends ComplexSeg {
 						cks.add(new Word(chs, senStartOffset, offset, 1));
 					}
 				}
+
+//				if(word.getLength() < 3) {
+//					cks.add(word);
+//				} else {
+//					char[] chs = word.getSen();
+//					int offset = word.getWordOffset(), n = 0, wordEnd = word.getWordOffset()+word.getLength();
+//					int senStartOffset = word.getStartOffset() - offset;	//sen 在文件中的位置
+//					int end = -1;	//上一次找到的位置
+//					for(; offset<wordEnd-1; offset++) {
+//						int idx = search(chs, offset, 1);
+//						if(idx > -1) {
+//							cks.add(new Word(chs, senStartOffset, offset, 2));
+//							end = offset+2;
+//							n++;
+//						} else if(offset >= end) {	//有单字
+//							cks.add(new Word(chs, senStartOffset, offset, 1));
+//							end = offset+1;
+//
+//						}
+//					}
+//					if(end > -1 && end < wordEnd) {
+//						cks.add(new Word(chs, senStartOffset, offset, 1));
+//					}
+//				}
 
 			}
 			chunk.words = cks.toArray(new Word[cks.size()]);
